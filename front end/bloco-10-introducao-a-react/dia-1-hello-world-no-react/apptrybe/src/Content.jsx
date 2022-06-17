65e4038ab4e1d4fbe2dc3ec403c8f7eaa0723121
Emily Menezes <estudosmenezes@gmail.com>
2022-06-17 18:05:50 -0300

    Dia 1 bloco 10

---
+++ front end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/apptrybe/src/Content.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './page.css'
+
+const conteudos = [
+  {
+    conteudo: 'High Order Functions',
+    bloco: 8,
+    status: 'Aprendido'
+  },
+  {
+    conteudo: 'Composição de Componentes',
+    bloco: 11,
+    status: 'Aprendendo',
+  },
+  {
+    conteudo: 'Composição de Estados',
+    bloco: 12,
+    status: 'Aprenderei'
+  },
+  {
+    conteudo: 'Redux',
+    bloco: 16,
+    status: 'Aprenderei'
+  },
+]
+
+class Content extends React.Component {
+  render() {
+    return(
+    <div className="content">
+    {conteudos.map((flag, index) => (
+        <div key={ index } className='list-content'>
+            <h4>{`O conteudo é: ${flag.conteudo}`}</h4>
+            <p>{`status: ${flag.status}`}</p>
+            <p>{`bloco: ${flag.bloco}`}</p>
+        </div>
+    ))}
+    </div>
+    );
+  }
+}
+
+export default Content;
---
+++ front end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/apptrybe/src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './page.css'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className='footer'>
+            <h1>Fim dos conteúdos de front end</h1>
+            </footer>
+    )
+  }
+}
+
+export default Footer;
