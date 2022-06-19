43a1ea3dfca610339164248feb354a256daa82b7
Emily Menezes <estudosmenezes@gmail.com>
2022-06-19 12:49:42 -0300

    Dia 1 Bloco 10

---
+++ front end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/classes-e-react/src/About.js
@@ -0,0 +1,21 @@
+function About() {
+    const name = <h1>Emily Menezes</h1>
+    const sobre =  
+    <p>
+        Eu sempre pensei que o meu sonho seria cursar alguma licenciatura na área de humanas e até mesmo por um momento cogitei - e me preparei - para ser uma policial militar e durante todo o meu ensino médio fiz listinhas de prós e contras para assim saber qual caminho eu deveria seguir. Entretanto, fui apresentada a área tecnológica por meio de um familiar e rapidamente acendeu-se uma chama que ardia imensamente por essa profissão.
+        E, a partir dai iniciei a minha saga de estudos nesse ambiente tão grande, mas, pouco falado. Em 2020 cursei um técnico em desenvolvimento de sistemas e tive o meu primeiro contato mais aprofundado com a área. Um ano depois, a Nubank fez uma parceria com a Cubos Academy e Alura e lançaram um programa que selecionaria pessoas do Estado da Bahia para receber um curso de desenvolvimento de software Back end com foco em Node.js, logo me interessei e participei do processo seletivo - que durou 4 meses - por fim, fui surpreendida com a notícia que fui aprovada para fazer parte da 1ª turma do NuLab. Durante seis meses eu me dediquei à tudo que essa oportunidade me proporcionou e agregou ao meu currículo. 
+        E então, no final do ano passado tomei conhecimento de uma instituição de ensino que preza pelas pessoas que querem construir uma carreira de sucesso em tecnologia, e notei que a trybe estava alinhada com a minha maior paixão: transformar a realidade por meio da tecnologia.
+    </p>
+  return (
+    <div>
+        {name} {sobre}
+    </div>
+  );
+}
+
+export default  About;
+
+
+
+
+
---
+++ front end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/classes-e-react/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import About from './About';
+import { Component } from 'react';
+
+class App extends Component {
+    render() {
+      return ( <About/>);
+    }
+  }
+  
+  export default App;
